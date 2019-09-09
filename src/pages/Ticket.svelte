77eca5c2a6cfa467e8fe968bff70ee942173ea46
lrgvdc9-1-1 <hchapa@lrgvdc911.org>
2019-09-09 11:52:51 -0500

    added messages

--- src/pages/Ticket.svelte
+++ src/pages/Ticket.svelte
@@ -80,15 +80,27 @@
                      viewers = spartans.filter((spartan) => (msg.users.includes(spartan.SOCKETID)));
                      console.log(viewers);
                 });
-
+               
+               //When users typing on the ticket
                socket.on("ticketInfo", (data) => {
                      console.log(data);
                      let form = data.data;
                    
-                     let element = document.getElementById(form.elemID)
+                     let formElement = document.getElementById(form.elemID)
+                     let spanElement = document.getElementById(form.elemID + "_span")
                      let userId  = data.user;
-                     element.disabled = form.locked;
-                     element.value = form.value;
+                     let full_name = viewers.find(spartan => (spartan.SOCKETID == userId));
+                     
+
+                     if(form.locked) {
+                        spanElement.textContent = `EDITING: ${full_name.FULL_NAME}`
+                     }else {
+                        spanElement.textContent = '';
+                     }
+                     
+
+                     formElement.disabled = form.locked;
+                     formElement.value = form.value;
                      viewers.forEach(spartan => {
                         if(spartan.SOCKETID == userId) {
                             spartan.editing.form = "TICKET";
@@ -102,6 +114,7 @@
                      viewers = viewers;
                });
 
+               //User kills the app un expectly update DOM
                socket.on("ticket:leaving", (data) => {
                      viewers = viewers.filter((spartan) => (spartan.SOCKETID != data.id))
                });
@@ -257,36 +270,43 @@
                            <div class="form-group">
                               <label>First Name</label>
                               <input class="inTicket" data-title="First Name" data-section="Customer Data" id="cfirst_name" type="text" />
-                              <span>MSG FROM SPARTAN CHAT</span>
+                              <span id="cfirst_name_span"></span>
                            </div>
                            <div class="form-group">
                               <label>Last Name</label>
                               <input class="inTicket" data-title="Last Name" data-section="Customer Data" id="clast_name" type="text" />
+                              <span id="clast_name_span"></span>
                            </div>
                            <div class="form-group">
                               <label>9-1-1 Telephone</label>
                               <input class="inTicket" data-title="9-1-1 Telephone" data-section="Customer Data" bind:this={ticket.tele_object}   id="telephone_land_line" type="text" />
+                              <span id="telephone_land_line_span"></span>
                            </div>
                            <div class="form-group">
                               <label>ALT 1 Tele</label>
                               <input class="inTicket" data-title="ALT 1 Tele" data-section="Customer Data" bind:this={ticket.alt_object}   id="alt_telephone"  type="text" />
+                              <span id="alt_telephone_span"></span>
                            </div>
                            <div class="form-group">
                               <label>ALT 2 Tele</label>
                               <input class="inTicket" data-title="ALT 2 Tele" data-section="Customer Data" bind:this={ticket.alt2_object}  id="alt2_telephone" type="text" />
+                              <span id="alt2_telephone_span"></span>
                            </div>
                            <div class="form-group">
                               <label>Email</label>
                               <input class="inTicket" data-title="Email" data-section="Customer Data" id="cemail" type="email"   />
+                              <span id="cemail_span"></span>
                            </div>
                            <div class="form-group">
                               <label>
                                  ALT E-mail
                               </label>
                               <input class="inTicket" data-title="ALT E-mail" data-section="Customer Data" id="alt_cemail" type="email" section="customer-data" />
+                              <span id="alt_cemail"></span>
                            </div>
                            <div class="form-group">
                               <label>Prefered Language</label>
+                              <span id="prf_language_span"></span>
                               <select class="inTicket" data-title="Prefered Language" data-section="Customer Data" id="prf_language"  section="customer-data" >
                                  <option></option>
                                  <option>English</option>
@@ -295,6 +315,7 @@
                            </div>
                            <div class="form-group">
                               <label>Walk In</label>
+                              <span id="walk_in_span"></span>
                               <select class="inTicket" data-title="Walk In" data-section="Customer Data" id="walk_in" section="customer-data" >
                                  <option ></option>
                                  <option >Yes</option>
@@ -303,6 +324,7 @@
                            </div>
                            <div class="form-group">
                               <label>Utilities</label>
+                              <span id="utility_span"></span>
                               <select class="inTicket" data-title="Utilities" data-section="Customer Data" id="utility" section="customer-data" >
                                  <option ></option>
                                  <option >Yes</option>
@@ -312,6 +334,7 @@
                            <div class="form-group">
                               <label>Mailing Info</label>
                               <input class="inTicket" data-title="Mailing Info" data-section="Customer Data" id="mailing_address"  type="text" section="customer-data" />
+                              <span id="mailing_address_span"></span>
                            </div>
                       </div>
                       <!-- END OF CUSTOMER DATA -->
@@ -319,42 +342,53 @@
                            <div class="form-group">
                               <label>Owner First Name</label>
                               <input class="inTicket" data-title="Owner First Name" data-section="Premises Data" id="pfirst_name"  type="text" />
+                              <span id="pfirst_name_span"></span>
                            </div>
                            <div class="form-group">
                               <label>Owner Last Name</label>
                               <input class="inTicket" data-title="Owner Last Name" data-section="Premises Data" id="plast_name" type="text" />
+                              <span id="plast_name_span"></span>
                            </div>
                            <div class="form-group">
                               <label >Subdivision</label>
                               <input class="inTicket" data-title="Subdivision" data-section="Premises Data" id="subdivision"  type="text" />
+                              <span id="subdivision_span"></span>
                            </div>
                            <div class="form-group">
                               <label>Block Number</label>
                               <input class="inTicket" data-title="Block Number" data-section="Premises Data" id="block_num" type="text" />
+                              <span id="block_num_span"></span>
                            </div>
                            <div class="form-group">
                               <label>Lot Number</label>
                               <input class="inTicket" data-title="Lot Number" data-section="Premises Data" id="lot_num" type="text" />
+                              <span id="lot_num_span"></span>
                            </div>
                            <div class="form-group">
                               <label>Tax Account</label>
                               <input class="inTicket" data-title="Tax Account" data-section="Premises Data" id="tax_account_num" type="text" />
+                              <span id="tax_account_num_span"></span>
                            </div>
                            <div class="form-group">
                               <label>Property Id</label>
                               <input class="inTicket" data-title="Property Id" data-section="Premises Data" id="property_id" type="text" />
+                              <span id="property_id_span"></span>
                            </div>
                            <div class="form-group">
                               <label>Street</label>
                               <input class="inTicket" data-title="Street" data-section="Premises Data" id="street" type="text" />
+                              <span id="street_span"></span>
                            </div>
                            <div class="form-group">
                               <label>Intersection</label>
                               <input class="inTicket" data-title="Intersection" data-section="Premises Data" id="intersection" type="text" />
+                              <span id="intersection_span"></span>
                            </div>
                            <div class="form-group">
                               <label>Building Description</label>
+                              <span id="building_description_span"></span>
                               <textarea class="inTicket" data-title="Building Description" data-section="Premises Data" id="building_description" ></textarea>
+                              
                            </div>
                         </div>
                         <!-- END OF PREMISES DATA -->
@@ -362,34 +396,42 @@
                            <div class="form-group">
                               <label>Address Number</label>
                               <input class="inTicket" data-title="Address Number" data-section="Location Validation" id="add_num" type="number" />
+                              <span id="add_num_span"></span>
                            </div>
                            <div class="form-group">
                               <label>Pre Directional</label>
                               <input class="inTicket" data-title="Pre Directional" data-section="Location Validation" id="prd" type="text" />
+                              <span id="prd_span"></span>
                            </div>
                            <div class="form-group">
                               <label>Road Name</label>
                               <input class="inTicket" data-title="Road Name" data-section="Location Validation" id="rd" type="text" />
+                              <span id="rd_span"></span>
                            </div>
                            <div class="form-group">
                               <label>Street Type</label>
                               <input class="inTicket" data-title="Street Type" data-section="Location Validation" id="sts" type="text" />
+                              <span id="sts_span"></span>
                            </div>
                            <div class="form-group">
                               <label>Post Directional</label>
                               <input class="inTicket" data-title="Post Directional" data-section="Location Validation" id="pod" type="text" />
+                              <span id="pod_span"></span>
                            </div>
                            <div class="form-group">
                               <label>Unit</label>
                               <input class="inTicket" data-title="Unit" data-section="Location Validation" id="unit" type="text" />
+                              <span id="unit_span"></span>
                            </div>
                            <div class="form-group">
                               <label>MSAG Comm</label>
                               <input class="inTicket" data-title="MSAG Comm" data-section="Location Validation"  id="msag_comm" type="text" />
+                              <span id="msag_comm_span"></span>
                            </div>
                            <div class="form-group">
                               <label>Postal Comm</label>
                               <input class="inTicket" data-title="Postal Comm" data-section="Location Validation" id="postal_comm" type="text" />
+                              <span id="postal_comm_span"></span>
                            </div>
                            <div class="form-group">
                               <label>Full Address</label>
@@ -399,11 +441,13 @@
                                     <button on:click={ticket.onClearFullAddress} class="button input-search-button"><span class="mif-bin"></span></button>
                                     <button on:click={ticket.onGlueFullAddress}  class="button input-search-button"><span class="mif-clipboard"></span></button>
                                  </div>
+                                 <span id="full_address_span"></span>
                               </div>
                            </div>
                            <div class="form-group">
                               <label>Address By</label>
                               <input id="address_by" data-title="Address By" data-section="Location Validation" class="inTicket"  placeholder="CLICK HERE TO STAMP" type="text" />
+                              <span id="address_by_span"></span>
                            </div>
                            <div class="form-group">
                               <DatePicker />
@@ -414,21 +458,25 @@
                            <div class="form-group">
                               <label>Range From</label>
                               <input class="inTicket" data-title="Range From" data-section="Database" id="range_low" type="number" />
+                              <span id="range_low_span"></span>
                            </div>
                            <div class="form-group">
                               <label>Range To</label>
                               <input class="inTicket" data-title="Range To" data-section="Database" id="range_high" type="number"  />
+                              <span id="range_high_span"></span>
                            </div>
                            <div class="form-group">
                               <label>
                                  ESN
                               </label>
                               <input class="inTicket" data-title="ESN" data-section="Database" id="esn" type="text" />
+                              <span id="esn_span"></span>
                            </div>
                            <div class="form-group">
                               <label>
                                  MSAG Valid
                               </label>
+                              <span id="msag_valid_span"></span>
                               <select id="msag_valid" data-title="MSAG Valid" data-section="Database" class="inTicket">
                                  <option></option>
                                  <option>YES</option>
@@ -440,12 +488,14 @@
                                  Trans ID
                               </label>
                               <input class="inTicket" data-title="Trans ID" data-section="Database"  id="trans_id" type="text" />
+                              <span id="trans_id_span"></span>
                            </div>
                            <div class="form-group">
                               <label>
                                  Verified Date
                               </label>
-                              <input class="inTicket" data-title="Verified Date"  data-section="Database" on:change={()=> {console.log(this.id); console.log("START"); console.log(this);}} id="date_verified" type="text" />
+                              <input class="inTicket" id="date_verified" data-title="Verified Date"  data-section="Database"   type="text" />
+                              <span id="date_verified_span"></span>
                            </div>
                      </div>
                      <!-- END OF DB -->
@@ -456,9 +506,11 @@
                                  Staff Initials
                               </label>
                               <input class="inTicket" data-title="Staff Initials" data-section="GIS" id="initials_mapping" type="text" />
+                              <span id="initials_mapping_span"></span>
                            </div>
                            <div class="form-group">
                               <input type="text" data-title="Date" data-section="GIS" id="mapping_verified_date" class="inTicket">
+                              <span id="mapping_verified_date_span"></span>
                            </div>
                            <br>
                            <hr>
@@ -468,21 +520,23 @@
                                  Staff Initials
                               </label>
                               <input class="inTicket" data-title="Staff Initials" data-section="GIS" id="initials_geocoding" type="text" />
+                              <span id="initials_geocoding_span"></span>
                            </div>
                            <div class="form-group">
-                              <input type="text" data-title="Geocoding Date" data-section="GIS" id="geocoding_date" class="inTicket">
+                              <input type="text" data-title="Geocoding Date" data-section="GIS" id="geocoding_date" class="inTicket">4
+                              <span id="geocoding_date_span"></span>
                            </div>
                      </div>
                      <!-- END OF GIS -->
             </div>
          </div>
-      <OnlineDash {isMe} {ontickets}/>
+      <OnlineDash {isMe} {socket} {ontickets}/>
 </div>
 
 
 <style>
   span {
-     color: red;
+     color: #D55441;
   }
    ul.tabs-list > li:hover {
       cursor: pointer;
@@ -512,5 +566,10 @@ select {
 }
 input {
    text-transform: uppercase;
+}
+
+input:disabled {
+  color:  #D55441;
+  cursor: wait !important;
 }
 </style>
