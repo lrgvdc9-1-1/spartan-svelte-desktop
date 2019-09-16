8d2f7389d771c849d74ef31943bb42a28ca133d3
lrgvdc9-1-1 <hchapa@lrgvdc911.org>
2019-09-16 15:19:21 -0500

    added toolbars for the online dash.

--- src/ui/OnlineDash.svelte
+++ src/ui/OnlineDash.svelte
@@ -30,8 +30,26 @@
     .name {
         color: #CAB448;
     }
+    .tool-button {
+        background: #243C73 !important;
+        color: white;
+    }
+    #close-button:hover {
+    background: #E81123;
+  }
 </style>
 <div style="float: right;width: 30%;height:100vh;background: #243C73;overflow:auto;">
+    <div style="width: 100%;height: 40px; background: #051740; border: 2px solid #CAB448;">
+        <div class="toolbar" style="float: right;" data-role="buttongroup"
+        data-mode="multi">
+            <button class="tool-button rounded">
+                  <span class="mif-arrow-up-right"></span>
+            </button>
+            <button class="tool-button rounded" id="close-button">
+                <span class="mif-exit"></span>
+            </button>
+        </div>
+    </div>
     <ul>
         {#each spartans as spartan, i}
              <!-- content here -->
