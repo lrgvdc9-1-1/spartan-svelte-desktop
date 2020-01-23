2b1c43f6b374b4df10b3d245791a0d336083681a
lrgvdc9-1-1 <hchapa@lrgvdc911.org>
2020-01-22 19:48:13 -0600

    Search Input UI

---
+++ src/ui/Archive/SearchInput.svelte
@@ -0,0 +1,29 @@
+ <script>
+    import {createEventDispatcher} from "svelte";
+    export let title;
+    let inValue;
+
+    const dispatch = createEventDispatcher();
+
+    function clearInput() {
+        inValue = "";
+    }
+
+    function onEnter() 
+    {
+
+        dispatch('search',inValue);
+    }
+ </script>
+ <div class="input">
+    <input type="text" bind:value={inValue} placeholder="{title}" data-role="input" data-search-button="true" data-role-input="true" class="">
+    <div class="button-group">
+        <button on:click="{clearInput}" class="button input-clear-button" tabindex="-1" type="button">
+        <span class="default-icon-cross"></span>
+        </button>
+        <button on:click="{onEnter}" class="button input-search-button" tabindex="-1" type="submit">
+        <span class="default-icon-search"></span>
+        </button>
+    </div>
+</div>
+             
