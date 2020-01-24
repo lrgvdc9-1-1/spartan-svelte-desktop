c32feac8e7e9026630d15978217c3f4fbfff5eea
lrgvdc9-1-1 <hchapa@lrgvdc911.org>
2020-01-23 19:47:53 -0600

    Features for menu on archive

--- src/pages/ArchiveTicketPage.svelte
+++ src/pages/ArchiveTicketPage.svelte
@@ -5,6 +5,10 @@
     import SQL from '../utils/sql';
     import Loading from '../ui/Loading.svelte';
     import SearchInput from "../ui/Archive/SearchInput.svelte";
+    import ArrowTicket from "../ui/Archive/ArrowTicket.svelte";
+    import ArrowSearch from "../ui/Archive/ArrowSearch.svelte";
+    import CalendarPopUp from '../ui/Archive/CalendarPopUp.svelte';
+    import ArrowUp from "../ui/Buttons/ArrowUp.svelte";
     export let isMe;
     
     export let url;
@@ -16,26 +20,45 @@
     let start = 0;
     let end   = 200;
     let loading = true;
-    let arrBDis = "none";
-    let slice = true;
-  
-    $: viewing = (slice) ? archives.slice(start, end) : archives;
-    onMount(() => {
-       
-        sql.getTicketStatus(['00']).then((res) => {
-            console.log(res)
-            archives = res.rows;
-            loading = false;
+    let arrs;
+    let arrt;
+    let calendar;
 
-        }).catch((error) => {
-            loading = false;
-            console.log(error);
-        })
+    $: viewing = archives.slice(start, end);
+
+    onMount(() => {
+        getAllArchives();    
 
     });
 
-    function redirectTicket(ticket) {
-        navigateTo(`#viewTicket/${ticket['id_ticket']}/${ticket['objectid']}`)
+    function getAllArchives(){
+        start = 0;
+        end = 200;
+        if(isMe['org_name'].includes('LRGVDC')) {
+            sql.getTicketStatus(['00']).then((res) => {
+                archives = res.rows;
+                loading = false;
+            }).catch((error) => {
+                loading = false;
+            })
+        }
+    }
+
+    function ticketOptions(ticket) {
+       // navigateTo(`#viewTicket/${ticket['id_ticket']}/${ticket['objectid']}`)
+       arrt.noHidden();
+       let data = arrt.getTicket();
+       if(data) {
+           if(data['id_ticket'] == ticket['id_ticket'] && data['objectid'] == ticket['objectid']){
+               arrt.onToggle();
+           }else if(arrt.getDisplay() == "none"){
+               arrt.onToggle();
+           }
+
+       }else {
+           arrt.onToggle();
+       }
+       arrt.setTicket(ticket)
     }
 
 
@@ -62,7 +85,7 @@
     function onDisplayMenu(e) {
         
         if(archives.length > 0) {
-            arrBDis = (arrBDis == "none") ? "block" : "none";
+            arrs.onToggle();
         }
 
     }
@@ -71,17 +94,34 @@
         start = 0;
         end = 200;
         arrBDis =  "none";
-        slice = true;
    
     }
 
 
     async function onSearch(e) {
-        slice = false;
         loading = true;
         var value = e.detail.toUpperCase();
         viewing = archives.filter((ticket) => ticket['cfirst_name'] == value || ticket['clast_name'] == value);
         loading = false;       
+    }
+
+    function onHandle(e) {
+        arrs.onClose();
+        switch (e.detail) {
+            case "REFRESH":
+                archives = [];
+                loading = true;
+                
+                getAllArchives();
+                break;
+            case "CALENDAR":
+                calendar.onToggle();
+                break;
+            case "ORGA":
+                break;
+            default:
+                break;
+        }
     }
 
 </script>
@@ -117,36 +157,8 @@
         color:  brown;
         text-decoration: underline;
     }
-
-    .arrow_box {
-        position: absolute;
-        box-shadow: 0 0 20px 0 rgba(0,0,0,.5);
-        background: white;
-        border: 4px solid #024775;
-        z-index:2;
-    }
-    .arrow_box:after, .arrow_box:before {
-        bottom: 100%;
-        left: 80%;
-        border: solid transparent;
-        content: " ";
-        height: 0;
-        width: 0;
-        position: absolute;
-        pointer-events: none;
-    }
-
-    .arrow_box:after {
-        border-color: rgba(136, 183, 213, 0);
-        border-bottom-color: white;
-        border-width: 20px;
-        margin-left: -20px;
-    }
-    .arrow_box:before {
-        border-color: rgba(194, 225, 245, 0);
-        border-bottom-color: #024775;
-        border-width: 26px;
-        margin-left: -26px;
+    #cog:hover {
+        background: #adacacbe !important;
     }
    
 </style>
@@ -165,65 +177,61 @@
              Showing {start + 1} of {end}
             </h3>
        </div>
-    <div id="body" style="background: white;border-radius: 8px; overflow: auto; overflow-x: hidden;height: 58vh;">
-    <div class="arrow_box" style="display: {arrBDis}; left:87%; top:80px">
-         <ul class="v-menu">
-            <li on:click|preventDefault="{onReset}"><a href="/#"><span class="mif-home icon"></span> Refresh Archive</a></li>
-            <li class="menu-title">Search</li>
-            <li><a ><span class="mif-calendar icon"></span> Calendar</a></li>
-            <li><a ><span class="mif-library icon"></span> Organization</a></li>
-            
-        </ul>
-    </div>
+    <ArrowTicket bind:this={arrt} />
+    <CalendarPopUp bind:this={calendar} />
+    <div id="body"  style="background: white;border-radius: 8px; overflow: auto; overflow-x: hidden;height: 58vh;">
+        
+        <ArrowSearch on:spit={onHandle} bind:this={arrs} />
        
-    <table class="table compact row-hover striped">
-        <thead>
-            <tr>
-                <th>Ticket #</th>
-                <th style="width: 125px;">Created Date</th>
-                <th>First Name</th>
-                <th>Last Name</th>
-                <th>Full Address</th>
-                <th>Property Id</th>
-                <th>Subdivision</th>
-
-                <th><button on:click="{onDisplayMenu}" class="button"><span class="mif-cog"></span></button>
-                     
-
-                </th>
-            </tr>
+            <table class="table compact row-hover striped">
             
-        </thead>
-        <tbody>
-         {#each viewing as ticket, i}
-               <tr>
-                    <td>
-                     <span on:click="{() => {redirectTicket(ticket)}}">{((ticket.objectid) ? ticket.objectid : ticket.id_ticket)}</span> 
-                    </td>
-                    <td>{FormatDate(ticket.created_date)}</td>
-                    <td>
-                        {ticket.cfirst_name}
-                    </td>
-                    <td>
-                        {ticket.clast_name}
-                    </td>
-                    <td>
-                        {ticket.full_address}
-                    </td>
-                    <td>
-                        {ticket.property_id}
-                    </td>
-                     <td>
-                        {ticket.subdivision}
-                    </td>
-                   
-              </tr>
-         {/each}
-        </tbody>
+                <thead>
+                    <tr>
+                        <th>Ticket #</th>
+                        <th style="width: 125px;">Created Date</th>
+                        <th>First Name</th>
+                        <th>Last Name</th>
+                        <th>Full Address</th>
+                        <th>Property Id</th>
+                        <th>Subdivision</th>
 
-    </table>    
+                        <th><button id="cog" on:click="{onDisplayMenu}" class="button"><span class="mif-cog"></span></button>
+                            
+
+                        </th>
+                    </tr>
+                    
+                </thead>
+                <tbody>
+                {#each viewing as ticket, i}
+                    <tr>
+                            <td>
+                            <span on:click="{() => {ticketOptions(ticket)}}">{((ticket.objectid) ? ticket.objectid : ticket.id_ticket)}</span> 
+                            </td>
+                            <td>{FormatDate(ticket.created_date)}</td>
+                            <td>
+                                {ticket.cfirst_name}
+                            </td>
+                            <td>
+                                {ticket.clast_name}
+                            </td>
+                            <td>
+                                {ticket.full_address}
+                            </td>
+                            <td>
+                                {ticket.property_id}
+                            </td>
+                            <td>
+                                {ticket.subdivision}
+                            </td>
+                        
+                    </tr>
+                {/each}
+                </tbody>
+
+            </table>    
     </div>
-    
+    <ArrowUp elemString="body" />
     <div class="footer">
         <div class="row-grid">
             <div>
