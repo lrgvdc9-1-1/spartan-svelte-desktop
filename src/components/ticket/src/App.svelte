50b2b23e83f7a2452261de5bd31c78248aec5f48
lrgvdc9-1-1 <hchapa@lrgvdc911.org>
2020-01-27 15:42:38 -0600

    ticket component development

---
+++ src/components/ticket/src/App.svelte
@@ -0,0 +1,15 @@
+<script>
+	
+</script>
+<ul class="tabs-list">
+    <li><a  href="/#">CUSTOMER DATA</a></li>
+    <li><a href="/#">PREMISES DATA</a></li>
+    <li><a href="/#">LOCATION VALIDATION PROCESS</a></li>
+	<li><a href="/#">DATABASE VALIDATION PROCESS</a></li>
+	<li><a href="/#">GIS VALIDATION PROCESS</a></li>
+	<li><a href="/#">ATTACHMENTS</a></li>
+	<li><a href="/#">COMMENTS FEED - </a></li>
+	<li><a href="/#">CONNECTIONS - </a></li>
+	<li><a href="/#">MESSAGES - </a></li>
+</ul>
+
